1df858066cc746b37d0830b1a1f4bebd32000e0d
komatsushoya <komatsushoya923@gmail.com>
2017-09-07 04:31:39 +0900

    1

--- addressbook.py
+++ addressbook.py
@@ -1,1 +1,19 @@
 class addressbook:
+    person_list =[]
+    def add(self, person):
+        self.person_list.append(person)
+
+    def show_all(self):
+        for person in self.person_list:
+            print(person.lastname + " " + person.firstname)
+
+    def serch(self,keyword):
+        for person in self.person_list:
+            if keyword in person.firstname or keyword in person.lastname:
+                print(person.firstname + " " + person.lastname)
+
+class Person:
+    import datetime
+
+    firstname = ''
+    lastname = ''
